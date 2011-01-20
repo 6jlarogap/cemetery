f41a5d8043ca9dd66b6a073dbc3e4c98c627a10a
Roman <pythonprogrammer@mail.ru>
2011-01-20 15:53:34 +0300

    Issue #333.

--- common/views.py
+++ common/views.py
@@ -841,7 +841,7 @@ def journal(request):
     today = datetime.date.today()
     burials = Burial.objects.filter(is_trash=False, creator=request.user,
                             date_of_creation__gte=datetime.datetime(year=today.year,
-                                month=today.month, day=today.day)).order_by('-date_of_creation', 'person__last_name')
+                            month=today.month, day=today.day)).order_by('-date_of_creation', 'person__last_name')[:50]
     return direct_to_template(request, 'journal.html', {'form': form, 'burials': burials})
 
 
