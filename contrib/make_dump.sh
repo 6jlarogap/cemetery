74a413e4fccaedbe29dbefc905db2702a2b1f7ac
Vitaliy Drozd <robotd@invozm.ru>
2011-03-01 15:16:37 +0300

    make dumps rsync

--- contrib/make_dump.sh
+++ contrib/make_dump.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source constants
 pg_dump -U postgres cemetery | gzip > $db_dumps_dir/"`eval date +%Y%m%d`".gz
+rsync -av /home/django/projects/cemetery/media/ofiles /var/cemetery/dumps/
