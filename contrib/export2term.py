18f4125ed231e19cec89a2562b73e77c5f6c4a6f
Vitaliy Drozd <robotd@invozm.ru>
2011-03-01 16:56:07 +0300

    export script modification НЕИЗВЕСТЕН безфамильный

--- contrib/export2term.py
+++ contrib/export2term.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+import stat
 from contrib.constants import *
 from common.models import Burial
 from django import db
@@ -14,32 +16,28 @@ fname = EXPORT2TERMINAL_FILE + '.partial'
 f = open(fname, "w")
 writer = csv.writer(f, "tabsep")
 
-total = burials.count()
-step_size = 1000
-for step in xrange(0, total, step_size):
-    for burial in burials[step:step + step_size]:
-        db.reset_queries()
-        uuid = burial.person.uuid
-        last_name = burial.person.last_name.encode('cp1251')
-        initials = burial.person.get_initials().encode('cp1251')
+for burial in burials.iterator():
+    person = burial.person
+    last_name = person.last_name
+    if (last_name) and not u'неизвестен' in last_name.lower() and not u'безфамильн' in last_name.lower():
+        uuid = person.uuid
+        place = burial.product.place
+        last_name = last_name.encode('cp1251')
+        initials = person.get_initials().encode('cp1251')
         if not initials:
             initials = u"-"
         bur_date = burial.date_fact
         if bur_date:
             b_date = bur_date.date()
             date = "%02d.%02d.%04d" %(b_date.day, b_date.month, b_date.year)
-#            date = bur_date.date().strftime("%d.%m.%Y")
-#        date = bur_date.date().isoformat()
-#        time = bur_date.time().strftime("%H:%M:%S")
         else:
             date = u"-"
-#        time = u"-"
-        area = burial.product.place.area.encode('cp1251')
-        row = burial.product.place.row.encode('cp1251')
-        seat = burial.product.place.seat.encode('cp1251')
-        cemetery = burial.product.place.cemetery.name.encode('cp1251')
-        if (last_name) and (last_name != "НЕИЗВЕСТЕН"):
-            writer.writerow((uuid, last_name, initials, date, area, row, seat, cemetery))
+        area = place.area.encode('cp1251')
+        row = place.row.encode('cp1251')
+        seat = place.seat.encode('cp1251')
+        cemetery = place.cemetery.name.encode('cp1251')
+        writer.writerow((uuid, last_name, initials, date, area, row, seat, cemetery))
+        db.reset_queries()
 f.close()
 os.chmod(fname, stat.S_IWOTH | stat.S_IROTH | stat.S_IWGRP | stat.S_IRGRP | stat.S_IRUSR | stat.S_IWUSR)
 os.rename(fname,EXPORT2TERMINAL_FILE)
--- contrib/export2vost.py
+++ contrib/export2vost.py
@@ -15,29 +15,28 @@ fname = EXPORT2TERMINAL_VOST_FILE + '.partial'
 f = open(fname, "w")
 writer = csv.writer(f, "vostochnoe")
 
-total = burials.count()
-step_size = 1000
-for step in xrange(0, total, step_size):
-    for burial in burials[step:step + step_size]:
-        db.reset_queries()
-        uuid = burial.person.uuid
-        last_name = burial.person.last_name.upper().encode('cp1251')
-        initials = burial.person.get_initials().upper().encode('cp1251')
+for burial in burials.iterator():
+    person = burial.person
+    last_name = person.last_name
+    if (last_name) and not u'неизвестен' in last_name.lower() and not u'безфамильн' in last_name.lower():
+        uuid = person.uuid
+        place = burial.product.place
+        last_name = last_name.upper().encode('cp1251')
+        initials = person.get_initials().upper().encode('cp1251')
         if not initials:
             initials = u"-"
         bur_date = burial.date_fact
         if bur_date:
             b_date = bur_date.date()
             date = "%02d.%02d.%04d" %(b_date.day, b_date.month, b_date.year)
-#            date = bur_date.date().strftime("%d.%m.%Y")
         else:
             date = u"-"
-        area = burial.product.place.area.encode('cp1251')
-        row = burial.product.place.row.encode('cp1251')
-        seat = burial.product.place.seat.encode('cp1251')
-        cemetery = burial.product.place.cemetery.name.encode('cp1251')
-        if (last_name) and (last_name.upper() != "НЕИЗВЕСТЕН"):
-            writer.writerow((uuid, last_name, initials, date, area, row, seat, cemetery))
+        area = place.area.encode('cp1251')
+        row = place.row.encode('cp1251')
+        seat = place.seat.encode('cp1251')
+        cemetery = place.cemetery.name.encode('cp1251')
+        writer.writerow((uuid, last_name, initials, date, area, row, seat, cemetery))
+        db.reset_queries()
 f.close()
 os.chmod(fname, stat.S_IWOTH | stat.S_IROTH | stat.S_IWGRP | stat.S_IRGRP | stat.S_IRUSR | stat.S_IWUSR)
 os.rename(fname,EXPORT2TERMINAL_VOST_FILE)
