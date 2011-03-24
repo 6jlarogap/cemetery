a3491c3ddd857a2dfe1c09077e035aab588ccc40
Vitaliy Drozd <robotd@invozm.ru>
2011-03-24 13:53:18 +0300

    makedump rsync delete files

--- contrib/make_dump.sh
+++ contrib/make_dump.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source constants
+#pg_dump -U postgres cemetery | gzip > /var/cemetery/dumps/"`eval date +%Y%m%d`".gz
 pg_dump -U postgres cemetery | gzip > $db_dumps_dir/"`eval date +%Y%m%d`".gz
-rsync -av /home/django/projects/cemetery/media/ofiles /var/cemetery/dumps/
+rsync -av --delete /home/django/projects/cemetery/media/ofiles /var/cemetery/dumps/
