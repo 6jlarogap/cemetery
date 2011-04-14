eef78a1c42b377da2d7851f4c95704c3f9fdd67a
Vitaliy Drozd <vitaliy.drozd@youmemory.org>
2011-04-14 19:27:51 +0400

    gp_views account_no patch populated for area, row and seat

--- contrib/pg_views.sql
+++ contrib/pg_views.sql
@@ -6,12 +6,12 @@ from common_burial;
 
 CREATE OR REPLACE VIEW common_place1
 AS select *, substring(area FROM '([^[:digit:]]*)[[:digit:]]*.*') AS s1,
-to_number(substring(area FROM '[^[:digit:]]*([[:digit:]]*).*'), '9999999999') AS s2,
+to_number(CASE WHEN substring(area FROM '[^[:digit:]]*([[:digit:]]*).*')='' THEN '9999999999' ELSE substring(area FROM '[^[:digit:]]*([[:digit:]]*).*') END, '9999999999') AS s2,
 substring(area FROM '[^[:digit:]]*[[:digit:]]*(.*)') AS s3,
 substring(row FROM '([^[:digit:]]*)[[:digit:]]*.*') AS s4,
-to_number(substring(row FROM '[^[:digit:]]*([[:digit:]]*).*'), '9999999999') AS s5,
+to_number(CASE WHEN substring(row FROM '[^[:digit:]]*([[:digit:]]*).*')='' THEN '9999999999' ELSE substring(row FROM '[^[:digit:]]*([[:digit:]]*).*') END, '9999999999') AS s5,
 substring(row FROM '[^[:digit:]]*[[:digit:]]*(.*)') AS s6,
 substring(seat FROM '([^[:digit:]]*)[[:digit:]]*.*') AS s7,
-to_number(substring(seat FROM '[^[:digit:]]*([[:digit:]]*).*'), '9999999999') AS s8,
+to_number(CASE WHEN substring(seat FROM '[^[:digit:]]*([[:digit:]]*).*')='' THEN '9999999999' ELSE substring(seat FROM '[^[:digit:]]*([[:digit:]]*).*') END, '9999999999') AS s8,
 substring(seat FROM '[^[:digit:]]*[[:digit:]]*(.*)') AS s9
 from common_place;
