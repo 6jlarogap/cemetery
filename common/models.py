ebfc056083829177d94a72b6d535358682cad136
Vitaliy Drozd <vitaliy.drozd@youmemory.org>
2011-07-11 19:01:06 +0400

    syntax fix

--- common/models.py
+++ common/models.py
@@ -266,7 +266,7 @@ class BankAccount(models.Model):
     Банковские реквизиты
     """
     organization = models.ForeignKey(Organization, verbose_name=u"Организация")      # Владелец счета
-    rs = models.CharField(u"Рассчетный счет", max_length=20, blank=True)             # Рассчетный счет
+    rs = models.CharField(u"Расчетный счет", max_length=20, blank=True)              # Расчетный счет
     ks = models.CharField(u"Корреспондентский счет", max_length=20, blank=True)      # Корреспондентский счет
     bik = models.CharField(u"БИК", max_length=9, blank=True)                         # Банковский идентификационный код
     bankname = models.CharField(u"Наименование банка", max_length=64, blank=True)    # Название банка
