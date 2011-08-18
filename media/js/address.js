1d58df7aca99e6fb29b973c5f239e65065997256
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2011-08-18 21:28:25 +0400

    #503

--- media/js/address.js
+++ media/js/address.js
@@ -15,57 +15,62 @@ $(function() {
 
   //автодополнение региона.
   $("input:text[id$=region]").autocomplete({
-        source: "/getregions/",
-        minLength: 2,
-        delay: 100,
-        select: function(event, ui) {
+       source: "/getregions/",
+       minLength: 2,
+       delay: 100,
+       select: function(event, ui) {
+            var id = $(this).attr('id').replace(/region/g, '');
             $(this).val(ui.item.value.split("/")[0]);
-            $(this).parents('tr').find('input:text[id$=country]').val(ui.item.value.split("/")[1]);
+            $('#'+id+'country').val(ui.item.value.split("/")[1]);
             return false;
        },
        focus: function(event, ui) {
             $(this).val(ui.item.value.split("/")[0]);
-            $(this).parents('tr').find('input:text[id$=country]').val(ui.item.value.split("/")[1]);
+            $('#'+id+'country').val(ui.item.value.split("/")[1]);
             return false;
        }
   });
   //автодополнение нас. пункта.
   $("input:text[id$=city]").autocomplete({
-        source: "/getcities/",
-        minLength: 2,
-        delay: 100,
-        select: function(event, ui) {
-            $(this).val(ui.item.value.split("/")[0]);
-            $(this).parents('tr').find('input:text[id$=region]').val(ui.item.value.split("/")[1]);
-            $(this).parents('tr').find('input:text[id$=country]').val(ui.item.value.split("/")[2]);
-            return false;
+       source: "/getcities/",
+       minLength: 2,
+       delay: 100,
+       select: function(event, ui) {
+           var id = $(this).attr('id').replace(/city/g, '');
+           $(this).val(ui.item.value.split("/")[0]);
+           $('#'+id+'region').val(ui.item.value.split("/")[1]);
+           $('#'+id+'country').val(ui.item.value.split("/")[2]);
+           return false;
        },
        focus: function(event, ui) {
+           var id = $(this).attr('id').replace(/city/g, '');
            $(this).val(ui.item.value.split("/")[0]);
-           $(this).parents('tr').find('input:text[id$=region]').val(ui.item.value.split("/")[1]);
-           $(this).parents('tr').find('input:text[id$=country]').val(ui.item.value.split("/")[2]);
+           $('#'+id+'region').val(ui.item.value.split("/")[1]);
+           $('#'+id+'country').val(ui.item.value.split("/")[2]);
            return false;
        }
 
   });
   //автодополнение улицы.
   $("input:text[id$=street]").autocomplete({
-        source: "/getstreets/",
-        minLength: 2,
-        delay: 100,
-        select: function(event, ui) {
-            $(this).val(ui.item.value.split("/")[0]);
-            $(this).parents('tr').find('input:text[id$=city]').val(ui.item.value.split("/")[1]);
-            $(this).parents('tr').find('input:text[id$=region]').val(ui.item.value.split("/")[2]);
-            $(this).parents('tr').find('input:text[id$=country]').val(ui.item.value.split("/")[3]);
-            return false;
+       source: "/getstreets/",
+       minLength: 2,
+       delay: 100,
+       select: function(event, ui) {
+           var id = $(this).attr('id').replace(/street$/g, '');
+           $(this).val(ui.item.value.split("/")[0]);
+           $('#'+id+'city').val(ui.item.value.split("/")[1]);
+           $('#'+id+'region').val(ui.item.value.split("/")[2]);
+           $('#'+id+'country').val(ui.item.value.split("/")[3]);
+           return false;
        },
        focus: function(event, ui) {
+           var id = $(this).attr('id').replace(/street$/g, '');
            $(this).val(ui.item.value.split("/")[0]);
-           $(this).parents('tr').find('input:text[id$=city]').val(ui.item.value.split("/")[1]);
-           $(this).parents('tr').find('input:text[id$=region]').val(ui.item.value.split("/")[2]);
-           $(this).parents('tr').find('input:text[id$=country]').val(ui.item.value.split("/")[3]);
-            return false;
+           $('#'+id+'city').val(ui.item.value.split("/")[1]);
+           $('#'+id+'region').val(ui.item.value.split("/")[2]);
+           $('#'+id+'country').val(ui.item.value.split("/")[3]);
+           return false;
        }
 
     });
