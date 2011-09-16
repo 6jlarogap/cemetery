c16000a41e44d5ad67054d87b64946208c1aaccb
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2011-09-16 10:40:59 +0400

    fabric

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+
+# sudo easy_install fabric
+
+from fabric.api import *
+
+env.hosts = ['youmemory.org']
+
+def deploy():
+    local('git push')
+    with cd('kaluga'):
+        run('sudo -u www-data git pull')
+        run('sudo -u www-data ./manage.py migrate')
+        run('sudo /etc/init.d/apache2 reload')
+
