b05543c81079476e6eabc21059d91487bfb1e91e
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-01-22 17:48:20 -0500

    copyfiles.sh script from MIL

--- contrib/copyfiles.sh
+++ contrib/copyfiles.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-#User for django
+
+sh ./netinstall.sh
+
+# User for django
 adduser --disabled-password --gecos "" django
 usermod -p $1$KQwQ4Rq7$CtkUF2cjpb9raBQZPjy4J0 django
-#Copy project files
+
+# Copy project files
 mkdir /home/django/projects
-#cp -R ../../cemetery /home/django/projects
+# cp -R ../../cemetery /home/django/projects
 git clone --no-hardlinks ../../cemetery /home/django/projects/cemetery
 mkdir -p /home/django/projects/cemetery/media/ofiles
 chown -R www-data:www-data /home/django/projects
-#Postgres
+
+# Postgres
 cp configs/postgres/pg_hba.conf /etc/postgresql/8.4/main/
 chown postgres:postgres /etc/postgresql/8.4/main/pg_hba.conf
 chmod 640 /etc/postgresql/8.4/main/pg_hba.conf
-/etc/init.d/postgresql restart
-#Nginx
+/etc/init.d/postgresql-8.4 restart
+
+# Nginx
 cp -R configs/nginx/fastcgi_params /etc/nginx/
 chown root:root /etc/nginx/fastcgi_params
 chmod 644 /etc/nginx/fastcgi_params
@@ -21,7 +27,8 @@ cp configs/nginx/cemetery /etc/nginx/sites-available/
 chown root:root /etc/nginx/sites-available/cemetery
 chmod 644 /etc/nginx/sites-available/cemetery
 ln -s /etc/nginx/sites-available/cemetery /etc/nginx/sites-enabled/
-#User for admin
+
+# User for admin
 adduser --disabled-password --gecos "" soul
 usermod -G admin soul
 usermod -p $1$KQwQ4Rq7$CtkUF2cjpb9raBQZPjy4J0 soul
@@ -31,38 +38,52 @@ chmod 700 /home/soul/.ssh
 cat configs/soul.pub >> /home/soul/.ssh/authorized_keys
 chown soul:soul /home/soul/.ssh/authorized_keys
 chmod 600 /home/soul/.ssh/authorized_keys
-#Django init script
+
+# Django init script
 cp configs/django /etc/init.d/
 chown root:root /etc/init.d/django
 chmod 751 /etc/init.d/django
 update-rc.d django defaults
-#Restore postgres dump
+
+# Restore postgres dump
 createdb -U postgres cemetery
-cat cemetery.sql | psql -U postgres cemetery
+cat basic.sql | psql -U postgres cemetery
 #Test strings
 #echo "*/1 * * * *    www-data   python /home/django/projects/cemetery/contrib/dumpdb.py 2>>/tmp/cronerror.txt" >> /etc/crontab
-#Database dump cron script
+
+# Database dump cron script
 echo "PYTHONPATH=/home/django/projects/cemetery:\$PYTHONPATH" >> /etc/crontab
 echo "DJANGO_SETTINGS_MODULE=settings" >> /etc/crontab
 echo "5 16 * * *    www-data   python /home/django/projects/cemetery/contrib/dumpdb.py" >> /etc/crontab
-echo "15 15 * * *    www-data   python /home/django/projects/cemetery/contrib/export2term.py" >> /etc/crontab
-#Start django daemon
-/etc/init.d/django restart
-/etc/init.d/nginx restart
-#Outbox
+
+# Outbox
 mkdir -p /var/cemetery/outbox
 mkdir -p /var/cemetery/inbox
 mkdir -p /var/cemetery/dumps
-mkdir -p /var/cemetery/terminal
 #mkdir -p /var/cemetery/media/ofiles
 chown -R www-data:www-data /var/cemetery
-chmod 777 /var/cemetery/terminal
-#--configs
-#cp configs/ubuntu/etc/network/interfaces /etc/network/interfaces
-#chown root:root /etc/network/interfaces
-#chmod 644 /etc/network/interfaces
-#/etc/init.d/networking restart
+
+# --configs
+# cp configs/ubuntu/etc/network/interfaces /etc/network/interfaces
+# chown root:root /etc/network/interfaces
+# chmod 644 /etc/network/interfaces
+# /etc/init.d/networking restart
+
 cp configs/ubuntu/etc/samba/smb.conf /etc/samba/smb.conf
 chown root:root /etc/samba/smb.conf
 chmod 644 /etc/samba/smb.conf
 /etc/init.d/smbd restart
+
+pip install -r ../pip_requirements.txt
+
+# Start django daemon
+/etc/init.d/django restart
+/etc/init.d/nginx restart
+
+cd /home/django/projects/cemetery/
+chmod +x ./manage.py
+
+./manage.py syncdb
+./manage.py migrate
+
+echo "Finished"
