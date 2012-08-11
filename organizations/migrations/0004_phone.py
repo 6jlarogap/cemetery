3448edaa01346e6bbda25698071628997dda03df
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-08-11 10:53:59 -0500

    Телефоны ФЛ и ЮЛ

---
+++ organizations/migrations/0004_phone.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Organization.phones'
+        db.add_column('organizations_organization', 'phones',
+                      self.gf('django.db.models.fields.TextField')(null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Organization.phones'
+        db.delete_column('organizations_organization', 'phones')
+
+
+    models = {
+        'geo.city': {
+            'Meta': {'unique_together': "(('region', 'name'),)", 'object_name': 'City', 'db_table': "'common_geocity'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Region']"})
+        },
+        'geo.country': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Country', 'db_table': "'common_geocountry'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'})
+        },
+        'geo.location': {
+            'Meta': {'object_name': 'Location'},
+            'block': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'building': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.City']", 'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Country']", 'null': 'True', 'blank': 'True'}),
+            'flat': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'gps_x': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'gps_y': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'post_index': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Region']", 'null': 'True', 'blank': 'True'}),
+            'street': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Street']", 'null': 'True', 'blank': 'True'})
+        },
+        'geo.region': {
+            'Meta': {'unique_together': "(('country', 'name'),)", 'object_name': 'Region', 'db_table': "'common_georegion'"},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Country']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        'geo.street': {
+            'Meta': {'ordering': "['city', 'name']", 'unique_together': "(('city', 'name'),)", 'object_name': 'Street'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.City']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        'organizations.agent': {
+            'Meta': {'object_name': 'Agent'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'organization': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'agents'", 'to': "orm['organizations.Organization']"}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['persons.Person']"})
+        },
+        'organizations.bankaccount': {
+            'Meta': {'object_name': 'BankAccount'},
+            'bankname': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'bik': ('django.db.models.fields.CharField', [], {'max_length': '9'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ks': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'ls': ('django.db.models.fields.CharField', [], {'max_length': '11', 'null': 'True', 'blank': 'True'}),
+            'organization': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['organizations.Organization']"}),
+            'rs': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'organizations.doverennost': {
+            'Meta': {'object_name': 'Doverennost'},
+            'agent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'doverennosti'", 'to': "orm['organizations.Agent']"}),
+            'expire_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'issue_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'organizations.organization': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Organization'},
+            'ceo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['persons.Person']", 'null': 'True', 'blank': 'True'}),
+            'ceo_document': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'ceo_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'ceo_name_who': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inn': ('django.db.models.fields.CharField', [], {'max_length': '12', 'blank': 'True'}),
+            'kpp': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '99'}),
+            'ogrn': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'phones': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'persons.person': {
+            'Meta': {'ordering': "['last_name', 'first_name', 'middle_name']", 'object_name': 'Person'},
+            'address': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geo.Location']", 'null': 'True'}),
+            'birth_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'birth_date_no_day': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'birth_date_no_month': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'death_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'middle_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'phones': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['organizations']
