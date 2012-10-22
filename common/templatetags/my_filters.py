787f65af3452b09d8101fb14676782ccc5333d82
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-10-21 19:58:56 -0500

    #765

--- common/templatetags/my_filters.py
+++ common/templatetags/my_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def in_group(user, group):
     return bool(user.is_superuser or user.groups.filter(name__iexact=group))
+
+@register.filter
+def has_perm(user, perm):
+    return bool(user.is_superuser or user.has_perm(perm))
